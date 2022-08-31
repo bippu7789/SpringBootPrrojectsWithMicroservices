e8cb6b7e27372029d39532f7e91234630a3de98a
Bipin kumar <bipin0007mca@gmail.com>
2022-08-31 12:59:13 +0530

    Single column data reflected remaining null.

--- BootDataJPAProj05-FINDERMETHOd-DynamicProjection/src/main/java/com/nt/runners/FinderMethodsTestRunners.java
+++ BootDataJPAProj05-FINDERMETHOd-DynamicProjection/src/main/java/com/nt/runners/FinderMethodsTestRunners.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import com.nt.entity.Actor;
 import com.nt.repository.IActorRepository;
 import com.nt.view.ResultView1;
 import com.nt.view.ResultView2;
@@ -30,8 +31,13 @@ public class FinderMethodsTestRunners implements CommandLineRunner {
 		System.out.println("Using result view3 as type interface");
 		Iterable<ResultView3> it3 = actorRepo.findByCategoryContainingIgnoreCase("A", ResultView3.class);
 		it3.forEach(view3 -> {
-			System.out.println(view3.getCategory() + "::" + view3.getAid()+" " + view3.getAname());
+			System.out.println(view3.getCategory() + "::" + view3.getAid() + " " + view3.getAname());
 		});
+
+		System.out.println("==============================SET NEW NAME");
+		Actor actor = new Actor();
+		actor.setAname("Ranveer");
+		System.out.println("Object save with actor repo" + actorRepo.save(actor).getAid() + "Value::");
 
 	}
 
